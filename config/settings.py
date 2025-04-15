bd68b95c479ef89cdf24be06178ebe4f7c64620c
Augusto <august.rosa@hotmail.com>
2025-04-15 03:28:01 -0400

    Updating Settings configuration script

--- config/settings.py
+++ config/settings.py
@@ -43,20 +43,3 @@ bucket_name = os.getenv('bucket_name')
 raw_path = os.getenv('raw_path')
 staging_path = os.getenv('staging_path')
 analytics_path = os.getenv('analytics_path')
-
-
-# from dotenv import load_dotenv
-# from pathlib import Path
-# import os
-
-# # Load environment variables from the .env file located in the project root
-# env_path = Path(__file__).resolve().parent.parent / '.env'
-# load_dotenv(dotenv_path=env_path)
-
-# # Variables used throughout thr project
-
-# # Environment variables
-# bucket_name = os.getenv('bucket_name')
-# raw_path = os.getenv('raw_path')
-# staging_path = os.getenv('staging_path')
-# analytics_path=os.getenv('analytics_path')
